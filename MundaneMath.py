dbd9516aa0254b6b3d442b372206f7a222f1f1f5
spasupu1 <54916075+spasupu1@users.noreply.github.com>
2019-09-17 13:15:26 -0400

    Delete MundaneMath.py

--- MundaneMath.py
+++
@@ -1,9 +0,0 @@
-
-print("The even numbers between 1 and 100 are")
-total=0	
-for num in range(2,100,2):
-# the value of num increases by a factor of 2 from 2 to 100
-#	print("{0}".format(num))
-	print(num)
-	total=total+num
-print ("The sum of even numbers between 1 and 100 is",total)
